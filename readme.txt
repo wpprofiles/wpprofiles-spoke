709200b5bc8a868577ead0d6fb4df3d89f297988
Richard Tape <richard.tape@ubc.ca>
2018-08-21 18:09:13 -0700

    Added initial commit. Just the boilerplate main plugin file and the readme.

---
+++ readme.txt
@@ -0,0 +1,6 @@
+Contributors: iamfriendly, jeremyfelt
+Donate link: https://charitywater.org
+Tags: profiles
+Requires at least: 4.9.0
+Tested up to: 4.9.8
+Stable tag: 0.0.1
---
+++ wpprofiles-spoke.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * WP PRofiles Spoke
+ *
+ * @package     WPPRofiles-Spoke
+ * @author      Richard Tape, Jeremy Felt
+ * @copyright   2018 WPProfiles
+ * @license     GPL-2.0+
+ *
+ * @wordpress-plugin
+ * Plugin Name: WPPRofiles Spoke
+ * Plugin URI:  https://wpprofiles.com/plugins/wpprofiles-spoke/
+ * Description: The Spoke for WP Profiles. Where folks display profiles from the Hub.
+ * Version:     0.0.1
+ * Author:      Richard Tape, Jeremy Felt
+ * Author URI:  https://wpprofiles.com/
+ * Text Domain: wpprrofiles-spoke
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
